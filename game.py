c63a6669b94676702e612b1b14b4c45bdba82179
Juan Fuenzalida <juan.fuenza97@gmail.com>
2022-09-14 14:39:03 -0300

    feat: zoom with keyboard and mousewheel

--- game.py
+++ game.py
@@ -61,8 +61,19 @@ class CameraGroup(pygame.sprite.Group):
         self.ground_surf = pygame.image.load('graphics/ground.png').convert_alpha()
         self.ground_rect = self.ground_surf.get_rect(topleft = (0,0))
 
+        # camera speed
         self.keyboard_speed = 5
         self.mouse_speed = 0.4
+
+        # zoom
+        self.zoom_scale = 1
+        self.internal_surf_size = (2500, 2500)
+        self.internal_surf = pygame.Surface(self.internal_surf_size, pygame.SRCALPHA)
+        self.internal_rect = self.internal_surf.get_rect(center = (self.half_w, self.half_h))
+        self.internal_surface_size_vector = pygame.math.Vector2(self.internal_surf_size)
+        self.internal_offset = pygame.math.Vector2()
+        self.internal_offset.x = self.internal_surf_size[0] // 2 - self.half_w
+        self.internal_offset.y = self.internal_surf_size[1] // 2 - self.half_h
 
     def center_target_camera(self, target):
         self.offset.x = target.rect.centerx - self.half_w
@@ -142,21 +153,38 @@ class CameraGroup(pygame.sprite.Group):
 
         self.offset += mouse_offset_vector * self.mouse_speed
 
+    def zoom_keyboard_control(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_q]:
+            self.zoom_scale += 0.1
+        if keys[pygame.K_e]:
+            self.zoom_scale -= 0.1
+            if self.zoom_scale < 0:
+                self.zoom_scale = 0.1
+
     def custom_draw(self, player):
 
         # self.center_target_camera(player)
         # self.box_target_camera(player)
         # self.keyboard_control()
         self.mouse_control()
+        self.zoom_keyboard_control()
+
+        self.internal_surf.fill('#71ddee')
 
         # ground
-        ground_offset = self.ground_rect.topleft - self.offset
-        self.display_surface.blit(self.ground_surf, ground_offset)
+        ground_offset = self.ground_rect.topleft - self.offset + self.internal_offset
+        self.internal_surf.blit(self.ground_surf, ground_offset)
 
         # active elements
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
-            offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
+            offset_pos = sprite.rect.topleft - self.offset + self.internal_offset
+            self.internal_surf.blit(sprite.image, offset_pos)
+
+        scaled_surf = pygame.transform.scale(self.internal_surf, self.internal_surface_size_vector * self.zoom_scale)
+        scaled_rect = scaled_surf.get_rect(center = (self.half_w, self.half_h))
+        
+        self.display_surface.blit(scaled_surf, scaled_rect)
 
 
 pygame.init()
@@ -185,6 +213,9 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+        
+        if event.type == pygame.MOUSEWHEEL:
+            camera_group.zoom_scale += event.y * 0.03
 
     ds.fill('#71ddee')
 
